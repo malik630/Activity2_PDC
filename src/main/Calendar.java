a012dac501203e30b486c9fd51c96dd12b224555
Khadidja <mk_foudili@esi.dz>
2025-12-11 09:37:22 +0100

    add facade

---
+++ src/main/Calendar.java
@@ -0,0 +1,7 @@
+package main;
+
+public class Calendar {
+    public Calendar() {}
+
+    public void calendarContent(){}
+}
---
+++ src/main/Encode.java
@@ -0,0 +1,7 @@
+package main;
+
+public class Encode {
+    public Encode(){}
+
+    public void encodeContent(){}
+}
